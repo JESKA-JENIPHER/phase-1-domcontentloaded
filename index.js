16889d66e4f2668e621052c9437eff0a954ff195
JESKA JENIPHER <jeska.jenipher@student.moringaschool.com>
2022-06-23 08:53:11 +0300

    commit tree 9c64d98463a8450e91625dad6d3eebb602b05ba2

--- index.js
+++ index.js
@@ -1,13 +1,5 @@
 // Your code goes here
-// document.addEventListener("DOMContentLoaded", function() {
-//   console.log("The DOM has loaded");
-// });
-
-// console.log(
-//   "This console.log() fires when index.js loads - before DOMContentLoaded is triggered"
-// );
-
-const event = document.addEventListener("DOMContentLoaded", function() {
-     document.getElementById("text").innerHTML = "This is really cool!"
+document.addEventListener('DOMContentLoaded', function(){
+    document.getElementById('text').textContent = 'This is really cool!';
 });
 
